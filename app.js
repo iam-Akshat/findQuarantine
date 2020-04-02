db58009938a540a57353ad8a548c3c52178b3d5e
Akshat Sethi <masterakshata@gmail.com>
2020-04-03 01:44:11 +0530

    fixed animation issue

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express= require('express');
+
+require('dotenv').config()
+
 const app=express();
 
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000,()=>console.log('Server started a'));
+app.use(express.json());//Used for parsing form data
+app.use(express.urlencoded({extended:true})); //Parse URL-encoded bodies
+
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+});
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.listen(process.env.PORT || 3000,()=>console.log('Server started at 3000'));
