a2eea223f6efc6619498f8e049749cdf29f1caac
Akshat Sethi <masterakshata@gmail.com>
2020-04-13 06:50:30 +0530

    completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const mongoose=require('mongoose');
-const dbHelper=require('./localmodules/dbhelper');
+const myFunction=require('./localmodules/dbhelper');
 require('dotenv').config()
 
 const app=express();
@@ -10,16 +10,35 @@ app.use(express.static('public'));
 app.use(express.json());//Used for parsing form data
 app.use(express.urlencoded({extended:true})); //Parse URL-encoded bodies
 
-
-
+//console.log(myFunction);    
+function lot(c){
+    console.log(c);
+}
 app.get('/',(req,res)=>{
     res.sendFile('index.html');
 });
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send({'MYDATA':'value','wfe':'FEWSDC'});
+app.get('/random',async (req,res)=>{
+    const data=await myFunction.getRandom();
+    console.log('what abuot to sned');
+    console.log(data);
+    console.log('what abuot to sned');
+    res.send(data);
 });
-app.listen(process.env.PORT || 3000,()=>{
-    console.log('Server started at 3000');
+app.post('/',async (req,res)=>{
+    
+    let tagsArray=req.body.intags.toLowerCase().split(',');
+    tagsArray.sort((a,b)=>a.length-b.length);
+    let obj={cid:req.body.cid,intags:tagsArray};
+    console.log(tagsArray);
+    try {
+        const toSend=await myFunction.doStuff(obj,process.env.DB_URL);
+        res.send(toSend);
+    } catch (error) {
+        console.log(error);
+    }
+    
 });
 
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Server started adsasdasdasdat 3000');
+});
--- localmodules/dbhelper.js
+++ localmodules/dbhelper.js
@@ -1,5 +1,4 @@
 const mongoose=require('mongoose');
-const tempUrl='mongodb+srv://admin-akshat:IVyDhLEFJVX1coO6@cluster0-1viwd.mongodb.net/findQ?retryWrites=true&w=majority';
 const personSchema=mongoose.Schema({
     cid:String,
     intags:Array
@@ -21,21 +20,96 @@ async function closeCon(){
         console.log('disconnected');
     })
 }
-async function doStuff(intags){
-    let t1=process.hrtime();
-    await init(tempUrl);
-    await Person.where({'intags':'sfa'}).find((err,res)=>{
-        if(!err) console.log(res); 
-    })
-    await closeCon();
-    let t2=process.hrtime(t1);
-    console.log("TIME TAKEN TO PREPARE A RESPONSE"+t2[0]+"s  "+t2[1]/1000000+"ms");
+async function getRandom(){
+    //const data={'sameTags':,'cid':cid}
+    console.log('RANDOM FUCNTION HERE');
+    return Person.find({}).then((foundPeople)=>{
+        console.log("list"+foundPeople);
+        const randomGuy=foundPeople[Math.floor(foundPeople.length-1)];
+        console.log(randomGuy)
+        const data={'sameTags':randomGuy.intags.slice(0,2),'cid':randomGuy.cid}
+        return data;
+    });
 }
+async function doStuff(obj,url){
+    const newUser=new Person(obj);
+
+    tagsArr=obj.intags;
+    try {
+        await init(url);
+    } catch (error) {
+        if(error);  
+    }
+    newUser.save();
+    console.log('This array was received in find function');
+    console.log(tagsArr);
+    const dat=Person.find({intags:{$in:tagsArr}}).then((val)=>{
+        if(val.length>0){
+            return processResult(tagsArr,val) ;
+        }else{
+            return {};
+        }
+        
+    });
+    return dat;
+    // Person.find({intags:{$in:tagsArr}},(err,res)=>{
+    //     if(!err){
+    //         //checks if any tags match at all
+    //         if(res.length>0){
+    //             console.log('step 3');
+    //             //let m=processResult(tagsArr,res).then((val)=>{return val});
+    //             //console.log(m);
+    //             return res;
+    //         }else{
+    //             console.log('step err');
+    //             return -1;
+    //         }
+    //     }
+    //     return 'why js why';
+    // });
+    // await closeCon();
+    // let t2=process.hrtime(t1);
+    // console.log("TIME TAKEN TO PREPARE A RESPONSE "+t2[0]+"s  "+t2[1]/1000000+"ms");
+}
+// let ar=['coding','sleeping'];
+// doStuff(ar);
 
 
+//compares array sent by current user with matching arrays
+//sent by db to find array with max common elements
+function processResult(tagsArr,resultAr){
+    // console.log('step processing found items');
+    let maxLen=0;
+    let cid;
+    let bestSet=new Set();   //stores set with most number of common
+    let tagSet=new Set(tagsArr);
+    let l=resultAr.length;
+    for(let t=0;t<l;t++){
+        let set=new Set(resultAr[t].intags);
+        let commonTags=intersection(tagSet,set);
+        if(commonTags.size>maxLen){
+            maxLen=commonTags.size;
+            bestSet=commonTags;
+            cid=resultAr[t].cid;
+        }
 
-
-// 
-
-
-module.exports={init};
+    }
+    const _h=Array.from(bestSet);
+    const data={'sameTags':_h,'cid':cid}
+    // console.log(_h);
+    console.log('Processign was done')
+    return data;
+    
+}
+///set intersection function
+function intersection(setA, setB) {
+    let _intersection = new Set()
+    for (let elem of setB) {
+        if (setA.has(elem)) {
+            _intersection.add(elem)
+        }
+    }
+    return _intersection;
+}
+//console.log(processResult(['a','bc','afas','atlos'],[{intags:['m','rt','afas']},{intags:['a','afas']}]));
+module.exports={doStuff,getRandom};
